4934215750a6a5261f2df1605bc4fededcdb67b1
Md Sirajul Munir <abc@gmail.com>
2023-10-31 00:46:03 +0600

    login registration ready

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,12 +1,25 @@
 import { Link, NavLink } from "react-router-dom";
 import userDefaultPic from "../../../assets/user.png";
+import { useContext } from "react";
+import { AuthContext } from "../../../Providers/AuthProviders";
+
+
 const Navbar = () => {
+
+  const {user, logOut} = useContext(AuthContext);
+  const handleSignOut = () =>{
+    logOut()
+    .then()
+    .catch()
+  }
+
     const navlinks=
     <>
         <li><NavLink to="/">Home</NavLink></li>
         <li><NavLink to="/about">About</NavLink></li>
         <li><NavLink to="/carrier">Carrier</NavLink></li>
         <li><NavLink to="/login">Login</NavLink></li>
+        <li><NavLink to="/register">Register</NavLink></li>
         
     </>
     return (
@@ -32,9 +45,14 @@ const Navbar = () => {
                 <img src={userDefaultPic} />
             </div>
       </label>
-    <Link>
+      {
+        user ?
+        <button onClick={handleSignOut} className="btn">Sign out</button>
+        :
+        <Link to="/login">
         <button className="btn">Login</button>
     </Link>
+      }
   </div>
 </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/Routes.jsx'
+import AuthProviders from './Providers/AuthProviders.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
+     <AuthProviders>
+        <RouterProvider router={router} />
+     </AuthProviders>
   </React.StrictMode>,
 )
